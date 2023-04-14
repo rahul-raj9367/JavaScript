78aeb905d11b39e57c448e36c3fb16b4a53df3e0
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-04-14 16:53:58 +0530

    Delete style.js

--- 2/style.js
+++
@@ -1,9 +0,0 @@
-//Write a program to calculate the area of a triangle.
-let breath=5;
-let height=12;
-let area=(breath*height)/2;
-console.log("Area of a Triangle is "+" "+area);
-/*
-OUTPUT
-Area of a Triangle is  30
-*/
