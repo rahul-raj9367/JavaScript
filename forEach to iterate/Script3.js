f805b6e897c775de8f8fd1865ca6c68242ae3165
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-04-15 12:14:11 +0530

    Add files via upload

---
+++ forEach to iterate/Script3.js
@@ -0,0 +1,13 @@
+//Create an array with five elements and then use forEach to iterate through the array and log each element to the console.
+const a=[10,20,30,40,50];
+a.forEach((num,index)=>{//num-parameters
+    console.log(index,num);
+})
+/*
+OUTPUT
+0 10
+1 20
+2 30
+3 40
+4 50
+*/
